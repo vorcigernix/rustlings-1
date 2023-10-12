57d2f6d56aac31e6325f702155f5bdd6cc8b5afa
Adam Sobotka <vorcigernix@gmail.com>
2023-10-12 19:42:31 +0000

    up to quiz 2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
